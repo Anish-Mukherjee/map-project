40cd3254cf5c4f957a52b8bca4f5e923b4195d4a
Anish <cybertronian217@gmail.com>
2023-03-27 16:20:26 +0530

    Add dynamic user location marker to map markers in MarkersMap component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
 import MarkersMap from "../components/MarkersMap";
+import { markers } from "../data/coordinates";
 
 export default function Home() {
   const [location, setLocation] = useState([-68.1193, -16.4897]);
+  const [mapMarkers, setMapMarkers] = useState(markers);
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(({ coords }) => {
         const { latitude, longitude } = coords;
         setLocation([longitude, latitude]);
+        setMapMarkers([...markers, { coordinates: location }]);
+        console.log(mapMarkers);
       });
     }
-  }, []);
+  }, [mapMarkers]);
   return (
     <div>
-      <MarkersMap coordinates={location} />
+      {mapMarkers ? <MarkersMap props={mapMarkers} /> : <>Loading...</>}
     </div>
   );
 }
