d433ae7432cccf7c46d98b89d9ea0d3ef47ef704
Anish <cybertronian217@gmail.com>
2023-03-27 15:54:03 +0530

    init

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import MarkersMap from "../components/MarkersMap";
+
+export default function Home() {
+  const [location, setLocation] = useState([-68.1193, -16.4897]);
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
+        setLocation([longitude, latitude]);
+      });
+    }
+  }, []);
+  return (
+    <div>
+      <MarkersMap coordinates={location} />
+    </div>
+  );
+}
